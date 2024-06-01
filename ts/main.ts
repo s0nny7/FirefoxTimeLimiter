ac70b15deffe94de807f1e13cf9a1bc5da8b2de1
JQTechnologies <pierwszy.jakubowiczor@gmail.com>
2024-06-01 22:17:25 +0200

    Styling

--- ts/main.ts
+++ ts/main.ts
@@ -26,6 +26,13 @@ function style() {
     style.id = "com-limitlost-limiter-style"
 
     style.innerHTML = `
+    #com-limitlost-limiter-panel table,#com-limitlost-limiter-panel tr,#com-limitlost-limiter-panel td{
+        border:unset;
+        outline:unset;
+        background-color:unset;
+        background:unset;
+    }
+
     #com-limitlost-limiter-panel {
         color: black;
         display: flex;
@@ -36,7 +43,7 @@ function style() {
         resize: both;
         z-index: 2147483647;
         border: 1px solid black;
-        background-color: rgba(255, 255, 255, 0.8);
+        background-color: rgba(230, 230, 230, 0.8);
         overflow: hidden;
         min-height: 2rem;
         min-width: 3rem;
@@ -87,6 +94,7 @@ function style() {
         overflow: scroll;
         flex-grow: 1;
         padding: 0.5rem;
+        width: fit-content;
     }
     `
 
@@ -105,7 +113,7 @@ function iconSvg() {
    <defs
       id="defs3" />
    <path
-  style="color:#000000;fill:#000000;stroke-linecap:round;stroke-linejoin:round;stroke:#000000;stroke-opacity:1;stroke-width:0.77043;stroke-dasharray:none;paint-order:normal;fill-opacity:1;stroke-dashoffset:0"
+  style="stroke-linecap:round;stroke-linejoin:round;stroke-opacity:1;stroke-width:0.77043;stroke-dasharray:none;paint-order:normal;fill-opacity:1;stroke-dashoffset:0"
       d="m 12,2.25 c -5.3758852,0 -9.75,4.3741148 -9.75,9.75 0,5.375922 4.3741188,9.75 9.75,9.75 5.375918,0 9.75,-4.374082 9.75,-9.75 0,-5.3758812 -4.374078,-9.75 -9.75,-9.75 z m 0,1.5 c 4.565268,0 8.25,3.6847711 8.25,8.25 0,4.565272 -3.684728,8.25 -8.25,8.25 C 7.4347711,20.25 3.75,16.565268 3.75,12 3.75,7.4347752 7.4347752,3.75 12,3.75 Z"
       id="path1" />
    <path
@@ -113,6 +121,44 @@ function iconSvg() {
   style="stroke-linecap:round;stroke-linejoin:round;stroke-opacity:1;stroke-width:0.77043;stroke-dasharray:none;paint-order:normal;fill-opacity:1;stroke-dashoffset:0"
       d="M 12 5.25 A 0.75 0.75 0 0 0 11.25 6 L 11.25 11.98623 A 0.75 0.75 0 0 0 11.46873 12.53127 L 15.708984 16.769531 A 0.75 0.75 0 0 0 16.769531 16.769531 A 0.75 0.75 0 0 0 16.769531 15.708984 L 12.75 11.687578 L 12.75 6 A 0.75 0.75 0 0 0 12 5.25 z " />
  </svg>`
+}
+
+function formatDuration(millis: number) {
+    let currentTime = millis;
+    //Tenths of a second
+    currentTime = Math.floor(currentTime / 100)
+    let secondTenths = currentTime % 10
+    //Seconds
+    currentTime = Math.floor(currentTime / 10)
+    let seconds = currentTime % 60
+    //Minutes
+    currentTime = Math.floor(currentTime / 60)
+    let minutes = currentTime % 60
+    //Hours
+    currentTime = Math.floor(currentTime / 60)
+    let hours = currentTime % 24
+    //Days
+    currentTime = Math.floor(currentTime / 24)
+    let days = currentTime
+
+    //Prefixes
+    let secondsPrefix = ""
+    if (seconds < 10) {
+        secondsPrefix = "0"
+    }
+    let minutesPrefix = ""
+    if (minutes < 10) {
+        minutesPrefix = "0"
+    }
+    //Format
+    let formatted = `${minutesPrefix}${minutes}:${secondsPrefix}${seconds}:${secondTenths}`
+    if (hours > 0 || days > 0) {
+        formatted = `${hours}:${formatted}`
+        if (days > 0) {
+            formatted = `${days}:${formatted}`
+        }
+    }
+    return formatted
 }
 
 function createPanel() {
